57ef4bc677a103d93244872c670444d1a3edc203
Phelipe <phelipelpos@gmail.com>
2023-06-23 17:19:39 -0300

    inicial commit

---
+++ src/TelaInicioConta.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+
+
+public class TelaInicioConta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String usuario = "Phelipe Oliveira";
+        double dinheiroConta = 2000;
+        double transferencia;
+        int opcao;
+        boolean interfaceRodando = true;
+
+
+        System.out.println();
+        System.out.println("Bem vindo! " + usuario);
+        System.out.println();
+
+        while (interfaceRodando){
+
+            System.out.println("Digite o número da operação que deseja fazer.");
+            System.out.println("1. Ver saldo atualizado.");
+            System.out.println("2. Fazer Transferência.");
+            System.out.println("3. Adicionar Saldo.");
+            System.out.println("4. Sair.");
+            System.out.println();
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Saldo atualizado de sua conta: R$" + dinheiroConta);
+                    System.out.println();
+                    break;
+                case 2:
+
+                    System.out.println("Digite o valor que deseja transferir ou digite 0 para cancelar: ");
+                    transferencia = scanner.nextDouble();
+
+                    if (transferencia<dinheiroConta && transferencia!=0){
+                            dinheiroConta -= transferencia;
+                        System.out.println();
+                        System.out.println("Transferência realizada com sucesso!");
+                        System.out.println("Saldo atualizado: R$" + dinheiroConta);
+                        System.out.println();
+                    }else if(transferencia>dinheiroConta){
+                        System.out.println();
+                        System.out.println("Você não tem valor o suficiente para essa transferência");
+                        System.out.println();
+                    }else{
+                        System.out.println();
+                        System.out.println("Operação cancelada.");
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println();
+                    System.out.println("Digite o saldo que deseja adicionar ou digite 0 para cancelar:");
+                    transferencia = scanner.nextDouble();
+                    if (transferencia<0) {
+                        System.out.println();
+                        System.out.println("Digite um valor maior que zero!");
+                        System.out.println();
+                    }else if (transferencia>0) {
+                        System.out.println("Saldo adicionado com sucesso!");
+                        dinheiroConta += transferencia;
+                        System.out.println("Saldo atualizado: R$" + dinheiroConta);
+                        System.out.println();
+                    }else{
+                        System.out.println();
+                        System.out.println("Operação cancelada.");
+                        System.out.println();
+                    }
+
+                    break;
+
+                case 4:
+                    interfaceRodando = false;
+
+            }
+            }
+        }
+        }
+
